d841fdc47bf31275de458cdad7dd33284f9c1895
= <neo.ahlander@outlook.com>
2025-04-02 20:31:18 +0200

    Added more endpoint templates

--- Api/Endpoints/AuthEndpoints.cs
+++ Api/Endpoints/AuthEndpoints.cs
@@ -7,6 +7,8 @@ public static class AuthEndpoints
         builder.MapPost("/auth/register", Register);
         builder.MapPost("/auth/login", Login);
         builder.MapPost("/auth/logout", Logout);
+        builder.MapPost("/auth/refresh", Refresh);
+        builder.MapGet("/auth/validate", Validate);
     }
 
     private static IResult Register()
@@ -20,6 +22,16 @@ public static class AuthEndpoints
     }
     
     private static IResult Logout()
+    {
+        return Results.Ok();
+    }
+    
+    private static IResult Refresh()
+    {
+        return Results.Ok();
+    }
+    
+    private static IResult Validate()
     {
         return Results.Ok();
     }
