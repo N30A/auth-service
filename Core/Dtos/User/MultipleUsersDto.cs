93990360d5a93c5f78575f89b2bc0767cf4c8134
= <neo.ahlander@outlook.com>
2025-04-07 16:02:26 +0200

    Refactor for new UserMapper

--- Core/Dtos/User/MultipleUsersDto.cs
+++ Core/Dtos/User/MultipleUsersDto.cs
@@ -1,26 +1,9 @@
-using Data.Models;
-
 namespace Core.Dtos.User;
 
 public class MultipleUsersDto
 {
-    public IEnumerable<UserDto> Users { get; } = [];
+    public IEnumerable<UserDto> Users { get; set; } = [];
     public int Count => Users.Count();
-    
-    private MultipleUsersDto() {}
-
-    public MultipleUsersDto(IEnumerable<UserModel> users)
-    {
-        Users = users.Select(user => new UserDto
-        {
-            Id = user.UserId,
-            Username = user.Username,
-            Email = user.Email,
-            CreatedAt = user.CreatedAt,
-            UpdatedAt = user.UpdatedAt,
-            DeletedAt = user.DeletedAt,
-        }).ToList();
-    }
     
     public static MultipleUsersDto Empty => new(); 
 }
