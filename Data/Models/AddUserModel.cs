316d9272ab7fd93ac9d2f3bf3fe4921cb03cb96e
= <neo.ahlander@outlook.com>
2025-05-11 12:01:43 +0200

    Added model for adding users

---
+++ Data/Models/AddUserModel.cs
@@ -0,0 +1,8 @@
+namespace Data.Models;
+
+public class AddUserModel
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+}
